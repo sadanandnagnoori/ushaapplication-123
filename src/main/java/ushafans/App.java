76fa2525763b03f22ab1c22464d623c0a5948c39
sadanand <sai@gmail.com>
2022-12-07 22:18:06 +0530

    new project

---
+++ src/main/java/ushafans/App.java
@@ -0,0 +1,13 @@
+package ushafans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
